cbe78366657d506d4db0569668d7e8a68adfaaaa
Some_one <hadassafavari48@gmail.com>
2025-12-02 20:39:30 -0300

    pronto

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import csv
+from datetime import datetime
 
 # Cria a aplicação Flask
 app = Flask(__name__)
@@ -34,6 +35,50 @@ def movie_details(movie_id):
     # Se não, retorna erro 404
     else:
         return "Movie not found!", 404
+    
+@app.get("/add-review")
+def show_add_form():
+    return render_template("add-review.html")
+    
+@app.post("/add-review")
+def add_review():
+
+    # 1. Busca o último ID adicionado (código que vimos acima)
+    with open("movies.csv", mode="r", encoding="utf-8") as csv_file:
+        reader = list(csv.reader(csv_file))
+        last_id = 0
+
+        if len(reader) > 1:
+            last_row = reader[-1]
+            if last_row:
+                last_id = int(last_row[0])
+
+        next_id = last_id + 1
+
+    # 2. Converte a data para o formato esperado
+    watched_date_raw = request.form.get("watched_date")
+    date_object = datetime.strptime(watched_date_raw, "%Y-%m-%d")
+
+    formatted_date = date_object.strftime("%m/%d/%Y")
+
+    # 3. Coleta os dados do formulário usando o atributo 'name' de cada campo
+    new_movie_data = [
+        next_id,
+        request.form.get("title"),
+        request.form.get("poster_url"),
+        request.form.get("rating"),
+        formatted_date,
+        request.form.get("review"),
+    ]
+
+    # 3. Anexa os novos dados ao arquivo CSV
+    with open("movies.csv", mode="a", encoding="utf-8", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(new_movie_data)
+
+    # 4. Redireciona para a página principal
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
